2e9fb429ed58ab324e4687f58e0fde76cf96d3d0
Jose Martinez <39181168+jomartinez27@users.noreply.github.com>
2022-08-11 10:08:21 -0500

    Add next-auth package, add prettierrc config file

--- pages/auth.js
+++ pages/auth.js
@@ -1,5 +1,24 @@
+import { getSession } from "next-auth";
+
 const AuthPage = () => {
-    return <div>
-        login
-    </div>
+  return <div>login</div>;
 };
+
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req });
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+}
+
+export default AuthPage;
